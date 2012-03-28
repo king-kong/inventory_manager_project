0f9948f4a9d70817d76cde8f6d0ba0b46ad1962e
Robert <mogilny@Robert-laptop.(none)>
2012-03-28 19:58:52 -0400

    added production database settings

--- application/models/products.php
+++ application/models/products.php
@@ -9,8 +9,9 @@ class Products extends CI_Model {
     {
         // Call the Model constructor
         parent::__construct();
-		$this->load->database();
-    }
+		$this->load->database('onzenit');
+    
+	}
 	
 	function get_products()
 	{
